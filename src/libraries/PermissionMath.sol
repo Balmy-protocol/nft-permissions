68be7b6020082a0803d0da5067e3ae1fcf01557d
Sam Bugs <sambugs@protonmail.com>
2023-11-14 16:46:14 -0300

    fix: set correct pragma versions

--- src/libraries/PermissionMath.sol
+++ src/libraries/PermissionMath.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.8;
 
 import { INFTPermissions } from "../interfaces/INFTPermissions.sol";
 import { NFTPermissions } from "../NFTPermissions.sol";
